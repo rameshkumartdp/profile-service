d693fb6ca919cd8222060ed1d9548f2ee0f4c440
Ramesh <rameshkumartdp@gmail.com>
2019-08-25 17:41:31 +0530

    - All the REST endpoints returns LIST now.

--- src/main/java/com/aktway/services/profile/resource/ServiceResource.java
+++ src/main/java/com/aktway/services/profile/resource/ServiceResource.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.inject.Inject;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -34,8 +36,9 @@ public class ServiceResource {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/followersprofile")
-    public FollowersProfileData getFollowersProfileData(@RequestParam("accessToken") String accessToken) {
+    public List<FollowersProfileData> getFollowersProfileData(@RequestParam("accessToken") String accessToken) {
 
+        List<FollowersProfileData> result = new ArrayList<>();
         followersProfileData.setProfileName("Narendra Manam");
         followersProfileData.setFollowersCount(10);
         followersProfileData.setFollowingCount(8);
@@ -43,26 +46,30 @@ public class ServiceResource {
         followersProfileData.isFriend();
         followersProfileData.setGender("M");
         followersProfileData.setProfilePicture("../../../assets/images/mockData/narendra_manam.jpg");
+        result.add(followersProfileData);
 
-        return followersProfileData;
+        return result;
     }
 
     @RequestMapping("/followingprofile")
-    public FollowingProfileData getFollowingProfileData(@RequestParam("accessToken") String accessToken) {
+    public List<FollowingProfileData> getFollowingProfileData(@RequestParam("accessToken") String accessToken) {
 
+        List<FollowingProfileData> result = new ArrayList<>();
         followingProfileData.setProfileName("Narendra Manam");
         followingProfileData.setFollowersCount(10);
         followingProfileData.setFollowingCount(8);
         followingProfileData.isFriend();
         followingProfileData.setGender("M");
         followingProfileData.setProfilePicture("../../../assets/images/mockData/narendra_manam.jpg");
+        result.add(followingProfileData);
 
-        return followingProfileData;
+        return result;
     }
 
     @RequestMapping("/followeespost")
-    public FolloweesPostData getFolloweesPostData(@RequestParam("accessToken") String accessToken) {
+    public List<FolloweesPostData> getFolloweesPostData(@RequestParam("accessToken") String accessToken) {
 
+        List<FolloweesPostData> result = new ArrayList<>();
         followeesPostData.setPostName("My new car");
         followeesPostData.setTagList(Arrays.asList("#Bangalore"));
         followeesPostData.setPostedBy("Narendra");
@@ -75,13 +82,15 @@ public class ServiceResource {
         followeesPostData.setPostedDate("Mon Jun 10 2019 13:45:44 GMT+0530 (India Standard Time)");
         followeesPostData.setUserProfilePic("../../../assets/images/mockData/narendra_manam.jpg");
         followeesPostData.setPostPicture("../../../assets/images/mockData/posts/car_image-2.jpg");
+        result.add(followeesPostData);
 
-        return followeesPostData;
+        return result;
     }
 
     @RequestMapping("/publicpost")
-    public MyPublicPostData getMyPublicPostData(@RequestParam("accessToken") String accessToken) {
+    public List<MyPublicPostData> getMyPublicPostData(@RequestParam("accessToken") String accessToken) {
 
+        List<MyPublicPostData> result = new ArrayList<>();
         myPublicPostData.setPostName("Beautiful bird");
         myPublicPostData.setLikeCount(16392);
         myPublicPostData.setCommentCount(2051);
@@ -89,19 +98,22 @@ public class ServiceResource {
         myPublicPostData.setPostId("MPUA0010");
         myPublicPostData.setPostedDate("Sun Feb 24 2019 17:15:44 GMT+0530 (India Standard Time)");
         myPublicPostData.setPostPicture("../../../assets/images/mockData/posts/post_bird-1.jpg");
+        result.add(myPublicPostData);
 
-        return myPublicPostData;
+        return result;
     }
 
     @RequestMapping("/privatepost")
-    public MyPrivatePostData getMyPrivatePostData(@RequestParam("accessToken") String accessToken) {
+    public List<MyPrivatePostData> getMyPrivatePostData(@RequestParam("accessToken") String accessToken) {
 
+        List<MyPrivatePostData> result = new ArrayList<>();
         myPrivatePostData.setPostName("Nice building");
         myPrivatePostData.setTagList(Arrays.asList("#Malaysia"));
         myPrivatePostData.setPostId("MPRA0006");
         myPrivatePostData.setPostedDate("Tue Jan 01 2019 12:00:44 GMT+0530 (India Standard Time)");
         myPrivatePostData.setPostPicture("../../../assets/images/mockData/posts/post_location-1.jpg");
+        result.add(myPrivatePostData);
 
-        return myPrivatePostData;
+        return result;
     }
 }
